ce0c31590ca8a14b72f342cf4877e53547ef8cca
Dante Cross <dantecross@outlook.com>
2021-01-05 04:59:38 +0000

    completed exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Enter Store Name:"
+@store_name = gets.chomp.to_s
+
+puts "Enter Stores annual revenue:"
+@store_revenue = gets.chomp.to_i
+
+@new_store = Store.create(
+  name: @store_name,
+  annual_revenue: @store_revenue
+)
+
+puts @new_store.errors.full_messages
